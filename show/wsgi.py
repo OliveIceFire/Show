c1a47eec21d02fd022bd5013bf086fb92fca35eb
zhxing_22c4 <zhxing2020@163.com>
2023-10-05 03:02:42 +0800

    Changes

--- show/wsgi.py
+++ show/wsgi.py
@@ -1,23 +1,69 @@
-"""
-WSGI config for show project.
+# This file contains the WSGI configuration required to serve up your
+# web application at http://ForShow.pythonanywhere.com/
+# It works by setting the variable 'application' to a WSGI handler of some
+# description.
+#
 
-It exposes the WSGI callable as a module-level variable named ``application``.
+# +++++++++++ GENERAL DEBUGGING TIPS +++++++++++
+# getting imports and sys.path right can be fiddly!
+# We've tried to collect some general tips here:
+# https://help.pythonanywhere.com/pages/DebuggingImportError
 
-For more information on this file, see
-https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
-"""
+
+# +++++++++++ HELLO WORLD +++++++++++
+# A little pure-wsgi hello world we've cooked up, just
+# to prove everything works.  You should delete this
+# code to get your own working.
+
+
+HELLO_WORLD = """<html>
+<head>
+    <title>PythonAnywhere hosted web application</title>
+</head>
+<body>
+<h1>Hello, World!</h1>
+<p>
+    This is the default welcome page for a
+    <a href="https://www.pythonanywhere.com/">PythonAnywhere</a>
+    hosted web application.
+</p>
+<p>
+    Find out more about how to configure your own web application
+    by visiting the <a href="https://www.pythonanywhere.com/web_app_setup/">web app setup</a> page
+</p>
+</body>
+</html>"""
+
+
+def application(environ, start_response):
+    if environ.get('PATH_INFO') == '/':
+        status = '200 OK'
+        content = HELLO_WORLD
+    else:
+        status = '404 NOT FOUND'
+        content = 'Page not found.'
+    response_headers = [('Content-Type', 'text/html'), ('Content-Length', str(len(content)))]
+    start_response(status, response_headers)
+    yield content.encode('utf8')
+
 
 import os
 import sys
 # Add your project's directory the PYTHONPATH
-# path = '/home/rangodemo2020/tango_with_django_project/'
-path = '/home/ForShow/app/'
 
+path = '/home/ForShow/Show/'
 if path not in sys.path:
     sys.path.append(path)
 # Move to the project directory
 os.chdir(path)
 # Tell Django where the settings.py module is located
+
+
+
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'show.settings')
+
+# You must either define the environment variable DJANGO_SETTINGS_MODULE or call settings.configure() before accessing settings.
 
 # Set up Django -- let it instantiate everything!
 import django
@@ -25,8 +71,72 @@ django.setup()
 # Import the Django WSGI to handle requests
 import django.core.handlers.wsgi
 application = django.core.handlers.wsgi.WSGIHandler()
-from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'show.settings')
 
-application = get_wsgi_application()
+# Below are templates for Django and Flask.  You should update the file
+# appropriately for the web framework you're using, and then
+# click the 'Reload /yourdomain.com/' button on the 'Web' tab to make your site
+# live.
+
+# +++++++++++ VIRTUALENV +++++++++++
+# If you want to use a virtualenv, set its path on the web app setup tab.
+# Then come back here and import your application object as per the
+# instructions below
+
+
+# +++++++++++ CUSTOM WSGI +++++++++++
+# If you have a WSGI file that you want to serve using PythonAnywhere, perhaps
+# in your home directory under version control, then use something like this:
+#
+#import sys
+#
+#path = '/home/ForShow/path/to/my/app
+#if path not in sys.path:
+#    sys.path.append(path)
+#
+#from my_wsgi_file import application  # noqa
+
+
+# +++++++++++ DJANGO +++++++++++
+# To use your own django app use code like this:
+#import os
+#import sys
+#
+## assuming your django settings file is at '/home/ForShow/mysite/mysite/settings.py'
+## and your manage.py is is at '/home/ForShow/mysite/manage.py'
+#path = '/home/ForShow/mysite'
+#if path not in sys.path:
+#    sys.path.append(path)
+#
+#os.environ['DJANGO_SETTINGS_MODULE'] = 'mysite.settings'
+#
+## then:
+#from django.core.wsgi import get_wsgi_application
+#application = get_wsgi_application()
+
+
+
+# +++++++++++ FLASK +++++++++++
+# Flask works like any other WSGI-compatible framework, we just need
+# to import the application.  Often Flask apps are called "app" so we
+# may need to rename it during the import:
+#
+#
+#import sys
+#
+## The "/home/ForShow" below specifies your home
+## directory -- the rest should be the directory you uploaded your Flask
+## code to underneath the home directory.  So if you just ran
+## "git clone git@github.com/myusername/myproject.git"
+## ...or uploaded files to the directory "myproject", then you should
+## specify "/home/ForShow/myproject"
+#path = '/home/ForShow/path/to/flask_app_directory'
+#if path not in sys.path:
+#    sys.path.append(path)
+#
+#from main_flask_app_file import app as application  # noqa
+#
+# NB -- many Flask guides suggest you use a file called run.py; that's
+# not necessary on PythonAnywhere.  And you should make sure your code
+# does *not* invoke the flask development server with app.run(), as it
+# will prevent your wsgi file from working.
