7dcee34536800cb77947acd639e67224442988eb
zhxing_22c4 <zhxing2020@163.com>
2023-10-05 01:32:53 +0800

    Changes

--- show/wsgi.py
+++ show/wsgi.py
@@ -8,7 +8,23 @@ https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 """
 
 import os
+import sys
+# Add your project's directory the PYTHONPATH
+# path = '/home/rangodemo2020/tango_with_django_project/'
+path = '/home/ForShow/app/'
 
+if path not in sys.path:
+    sys.path.append(path)
+# Move to the project directory
+os.chdir(path)
+# Tell Django where the settings.py module is located
+
+# Set up Django -- let it instantiate everything!
+import django
+django.setup()
+# Import the Django WSGI to handle requests
+import django.core.handlers.wsgi
+application = django.core.handlers.wsgi.WSGIHandler()
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'show.settings')
